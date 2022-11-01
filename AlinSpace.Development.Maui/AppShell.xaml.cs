846d6d8f8b0064dce2d91d16cbad9902b61f30d6
Alin Andersen <alin.porcic@gmail.com>
2022-11-01 22:22:16 +0100

    Updates.

---
+++ AlinSpace.Development.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AlinSpace.Development.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
